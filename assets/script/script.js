b4b78066bcd41a0c3b7ef32b8caf0e071b9bf29e
Dmytriy99 <dima.sharov99.ds@gmail.com>
2023-05-05 12:31:28 +0200

    fix

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,14 @@
 const btnBox = document.querySelector(".btn-Box");
 const contBox = document.querySelector(".container");
 const resetBox = document.querySelector(".reset-box");
-
+//funzione per manipolare il DOM
 function creationHandler(tag, style, content) {
   button = document.createElement(tag);
   button.className = style;
   button.innerHTML = content;
   return button;
 }
-
+// lo style è vuoto perchè utilizzo già nel css sono il tag "button"
 const increseBtn = creationHandler("button", "", "+");
 const decreseBtn = creationHandler("button", "", "-");
 const resetBtn = creationHandler("button", "", "RESET");
@@ -17,6 +17,7 @@ resetBox.append(resetBtn);
 
 btnBox.append(decreseBtn, textP, increseBtn);
 
+// funzione di calcolo
 let counter = 0;
 function target(e) {
   if (e.target === increseBtn) {
